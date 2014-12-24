1b089cbfb195827164c76e169ee5826bc533ba82
Will Hopper <whopper@puppetlabs.com>
2014-12-24 07:11:03 +0000

    Move main report to its own template

--- application.rb
+++ application.rb
@@ -3,3 +3,14 @@ require 'sinatra'
 get '/' do
   erb :index
 end
+
+post '/' do
+  @totalEarnedCredits = 180
+  @totalInProgressCredits = 13
+  @totalNeededCredits = 10
+  @upperDivEarnedCredits = 70
+  @upperDivInProgressCredits = 13
+  @upperDivNeededCredits = 4
+  #params[:content]
+  erb :index
+end
